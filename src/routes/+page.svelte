0d718bed2419c3d4769d94afc94036135475365a
bouldersky <skyarnold1@me.com>
2023-07-26 22:27:16 -0600

    setup svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let countryData = {
+    name: "United States",
+    
+  }
+</script>
+
+
+<main class="flex items-center justify-center">
+	<h1 class="text-8xl">Pwerdle</h1>
+  
+</main>
