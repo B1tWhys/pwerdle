347074388a38134dce919cdc794f677f33fe3aac
bouldersky <skyarnold1@me.com>
2023-07-29 19:25:20 -0600

    loading country from api works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+	// import { hierarchy } from 'd3-hierarchy';
 	import type { PageData } from './$types';
+
 	export let data: PageData;
 	const country = data.country;
+	let i = 0;
+
+	// hierarchy(country);
 </script>
 
 <main class="flex items-center justify-center flex-col">
 	<h1 class="text-8xl">Pwerdle</h1>
-	<h2 class="">{country.title}</h2>
+	<h2>{country.title}</h2>
+	<button on:click={() => (i += 1)}>{i}</button>
 </main>
--- src/routes/todaysCountry/+server.ts
+++ src/routes/todaysCountry/+server.ts
@@ -7,8 +7,10 @@ import { json } from '@sveltejs/kit';
 export const GET = (() => {
 	const today = new Date();
 	const daysSinceEpoch = Math.floor(today.getTime() / (60 * 60 * 24));
+	const seed = String(daysSinceEpoch);
+	console.debug(`Seeing rng with: ${seed}`);
 
-	random.use(seedrandom(String(daysSinceEpoch)) as unknown as RNG);
+	random.use(seedrandom(seed) as unknown as RNG);
 	const country = random.choice(countries);
 
 	return json(country);
